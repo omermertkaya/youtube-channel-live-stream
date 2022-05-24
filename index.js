a053a7b7f7c511ee7a88694e70a0780685a86261
omermertkaya <omermertkaya@hotmail.com.tr>
2022-05-24 14:15:33 +0300

    update
    update

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const axios = require('axios');
 
-async function getLiveStream(url) {
+async function getLiveStream(url,isName) {
     var liveStream = []
-    const getURL = 'https://www.youtube.com/c/'+ url +'/videos?view=2&live_view=501'
+    var getURL
+    if(isName == true){
+        getURL = 'https://www.youtube.com/c/'+ url +'/videos?view=2&live_view=501'
+    }else if(isName == false){
+        getURL = 'https://www.youtube.com/channel/'+ url +'/videos?view=2&live_view=501'
+    }
     const response = await axios.get(getURL)
 
     var getData = response.data
     var liveVideoIDTag = 'var ytInitialData = '
     let position = getData.indexOf(liveVideoIDTag)
     var positionEnd = getData.indexOf('</script>', position)
-    // console.log(position)
-    // console.log(getData.slice(position+20,positionEnd-1))
     var gettingData = JSON.parse(getData.slice(position+20,positionEnd-1))
-    // console.log(gettingData)
-    // console.log(gettingData.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents)
-    
     var searchArray = gettingData.contents.twoColumnBrowseResultsRenderer.tabs[1].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].gridRenderer.items
 
     for (let index = 0; index < searchArray.length; index++) {
